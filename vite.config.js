f7a2cbc6d9e1779fa6e746ca71d71d0328e9bf53
Syed-Sheharyar <96941588+Syed-Sheharyar@users.noreply.github.com>
2023-05-31 11:30:44 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import { react } from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      "/v2/top-headlines": "https://newsapi.org",
-      "/v2/everything": "https://newsapi.org"
+      "/v2": "https://newsapi.org"
     }
   },
   plugins: [react]
